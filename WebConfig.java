de06af7abb153c379e36716b6675070819753a0c
arcoro <39470933+arcoro@users.noreply.github.com>
2018-05-21 18:21:06 -0500

    Add files via upload

---
+++ WebConfig.java
@@ -0,0 +1,8 @@
+package com.spring.jms.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+}
